ec091134bf2ef94387ab23a3be7156aa59adfe97
warrenwoolseyiii <warren.woolseyiii@gmail.com>
2018-03-10 20:57:57 -0800

    Fully implemented the beginner mode algo, created a new release and tested in the oil field demo

---
+++ Scripts/Example Usage Scripts/motusMovement.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+using Valve.VR;
+ 
+[RequireComponent(typeof(CharacterController))]
+public class motusMovement : MonoBehaviour
+{
+    //player is the camerarig
+    public GameObject player;
+    private CharacterController playerController;
+    SteamVR_Controller.Device device;
+    SteamVR_TrackedObject controller;
+    public float speedMultiplier = 10f;
+    public float playerGravity = 10f;
+    private AutoOrienter _autoOrienter = new AutoOrienter();
+    Vector2 touchpad;
+
+    void Start()
+    {
+        //Call to initialize the Motus controller
+        MotusInput.Start();
+        playerController = player.GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MotusInput.Update();
+
+        Vector3 trans = MotusInput.GetNormalizedTranslation();
+
+        Quaternion tracker = RotationTracker.GetRotation();
+
+        if (!_autoOrienter.IsOriented())
+            _autoOrienter.Orient(tracker);
+
+        if (trans.magnitude == 0.0f)
+            MotusInput.SetNewSteeringOffset(tracker);
+
+        Quaternion rot = MotusInput.GetPlayerRotation(tracker);
+        trans = MotusInput.GetTrim() * trans;
+        trans = rot * trans;
+        trans *= speedMultiplier;
+        trans += new Vector3(0, -1 * playerGravity, 0);
+
+        if (playerController.isGrounded)
+        {
+            playerController.Move(trans * Time.deltaTime);
+            player.transform.localRotation = new Quaternion(0, 0, 0, 1);
+        }
+        else
+        {
+            makePlayerFall();
+        }
+    }
+
+    private void makePlayerFall()
+    {
+        playerController.Move(new Vector3(0, -1 * playerGravity * Time.deltaTime, 0));
+    }
+}
