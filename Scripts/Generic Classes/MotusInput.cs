9e26f0ba723ee5da6395b87c14fdd8b039f5539c
warrenwoolseyiii <warren.woolseyiii@gmail.com>
2018-03-10 20:12:07 -0800

    Implemented the beginner algorithm in the scripts. This is embodied in a function called trim, which allows the user to use the 3 pads infront of them to move forward which eases use.

--- Scripts/Generic Classes/MotusInput.cs
+++ Scripts/Generic Classes/MotusInput.cs
@@ -8,6 +8,9 @@ public static class MotusInput
     static private Motus_1_Platform _platform = new Motus_1_Platform();
     static private Quaternion _steeringOffset = new Quaternion(0, 0, 0, 1);
     static private Quaternion _inGameOffset = new Quaternion(0, 0, 0, 1);
+    static private Quaternion _trim = new Quaternion(0, 0, 0, 1);
+    static private bool _isMoving = false;
+    static private bool _movingStateChange = false;
 
     // Use this for initialization
     public static void Start()
@@ -23,12 +26,37 @@ public static class MotusInput
         _platform = Motus1.GetRawPlatformData();
     }
 
+    // Use this to get the _inGameOffset field applied to the motus
+    public static Quaternion GetInGameOffset()
+    {
+        return _inGameOffset;
+    }
+
     // Use this to get a normalized translation vector in the x & z plane
     public static Vector3 GetNormalizedTranslation()
     {
         Motus_1_MovementVector local = _vector;
         local.Normalize();
-        return new Vector3(local.LateralComponent, 0f, local.VerticalComponent);
+        Vector3 rtn = new Vector3(local.LateralComponent, 0f, local.VerticalComponent);
+
+        if (rtn.magnitude != 0)
+        {
+            if (!_isMoving)
+            {
+                _movingStateChange = true;
+                _isMoving = true;
+            }
+        }
+        else
+        {
+            if (_isMoving)
+            {
+                _movingStateChange = true;
+                _isMoving = false;
+            }
+        }
+
+        return rtn;
     }
 
     // Use this to get a raw data object with all of the Motus-1 data fields
@@ -48,6 +76,35 @@ public static class MotusInput
         Quaternion steering = rotationTracker * _steeringOffset;
         rtn = _inGameOffset * steering;
         return rtn;
+    }
+
+    // Use this method to get the easy mode algorithm to trim your motion vector
+    public static Quaternion GetTrim()
+    {
+        if (_movingStateChange)
+        {
+            _movingStateChange = false;
+
+            if (_isMoving)
+            {
+                Motus_1_MovementVector local = _vector;
+                local.Normalize();
+                Vector3 trans = new Vector3(local.LateralComponent, 0f, local.VerticalComponent);
+                Quaternion motusRot = _inGameOffset * Quaternion.LookRotation(trans);
+                Quaternion deltaQuat = motusRot * GetSteeringOffset();
+                float delta = deltaQuat.eulerAngles.y;
+                if (delta > 180f)
+                    delta -= 360f;
+                if (Mathf.Abs(delta) <= 65f)
+                    _trim = Quaternion.Euler(0f, -delta, 0f);
+                else
+                    _trim = Quaternion.Euler(0f, 0f, 0f);
+            }
+            else
+                _trim = Quaternion.Euler(0f, 0f, 0f);
+        }
+
+        return _trim;
     }
 
     // Use this to get the player rotation vector from your rotation tracker source
